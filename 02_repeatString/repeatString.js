e35a590aea61d0185d391484ef39e43851a6554a
Daisy-Opanga <daisyopanga2@gmail.com>
2024-10-09 10:15:05 +0300

    modified:   01_helloWorld/helloWorld.js modified:   02_repeatString/repeatString.js modified:   02_repeatString/repeatString.spec.js modified:   03_reverseString/reverseString.js modified:   03_reverseString/reverseString.spec.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,21 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+let repeatedStr;
+if (num === 0) {
+    repeatedStr = "";
+} else if (num < 0) {
+    repeatedStr = "ERROR";
+}else {
+    for (let i = 1 ; i <= num; i++) {
+        if (i === 1){
+           repeatedStr = string;
+        } else{
+           repeatedStr = repeatedStr + string;
+        } 
+   }
+}
+return repeatedStr;
 };
+repeatString("hey",3);
 
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(myString) {
+let arrayReverse = () => {
+    return myString
+    .split("")
+    .reverse();
 };
+return arrayReverse().join("");
+};
+reverseString("daisy");
+console.log("Hi");
 
 // Do not edit below this line
 module.exports = reverseString;
+
