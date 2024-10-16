22ec36cc09d7e069300f75b9af8a0a8a882c60f9
Daisy-Opanga <daisyopanga2@gmail.com>
2024-10-16 22:25:28 +0300

    Did this exercise and passed all tests modified:   05_sumAll/sumAll.js modified:   05_sumAll/sumAll.spec.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,22 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    let larger, smaller;
+    
+    if(typeof num1 !== "number" || typeof num2 !== "number" || !Number.isInteger(num1) ||
+    !Number.isInteger(num2) || num1 < 0 || num2 < 0) {
+       return "ERROR"; 
+    } else{
+        num1 < num2 ? larger = num2 : larger =num1;
+        num1 < num2 ? smaller = num1 : smaller = num2;
+        // alternatively use: 
+        // let larger = Math.max(num1, num2);
+        // let smaller = Math.min(num1, num2);
+        let x = smaller;
+        for ( x; x <= larger; x++) {
+            sum = sum + x ; 
+         }
+         return sum;
+    } 
 
 };
 
