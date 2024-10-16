81c4e3af5551cb4f3721c7bcb1a3ce1cc0fef637
Daisy-Opanga <daisyopanga2@gmail.com>
2024-10-16 08:30:34 +0300

    Did the exercise sucessfully modified:   04_removeFromArray/removeFromArray.js modified:   04_removeFromArray/removeFromArray.spec.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...arg) {
+    // let finalArr = arr;
+    //     for (let x of arg){
+    //         finalArr = finalArr.filter((val) => val !== x);
+    //     }
+    //     return finalArr;
+    return arg.reduce((arr, argX) => {
+        return arr.filter((arrVal) => arrVal !== argX);
+    }, arr);
 };
 
 // Do not edit below this line
